97692cdfc9a2625bdacb674b22ac82076d527a5f
Ramya <nramya1996@gmail.com>
2017-03-28 02:55:56 +0530

    Android Assignment 3.2
    UI for a Credit Card application that will help people with their credit
    card debt.

---
+++ Assignment3.2/app/src/main/java/com/example/ramya/assignment32/FourthActivity.java
@@ -0,0 +1,14 @@
+package com.example.ramya.assignment32;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FourthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth);
+    }
+
+}
